c896c8384bfcd2f4d3b004a662f7733b74718b3c
Lawrence Thomas <lawrence@macbookpro.mynetworksettings.com>
2025-04-18 18:34:08 -0400

    updates to gf duties page

---
+++ page-girlfriend-duties-list.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<div class="c-page-girlfriend-duties-list">
+    <h2 class="c-page-girlfriend-duties-list__title"><?php echo get_the_title(); ?></h2>
+    <div class="c-page-girlfriend-duties-list__content">
+    <?php echo the_content(); ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
